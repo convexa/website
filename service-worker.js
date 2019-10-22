a43c7c6b7381a79db4b186f7abfe514ca0741e4a
Sebastian Sassi <ssassi@sophilabs.com>
2019-10-22 18:04:56 -0300

    Update texts.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/themes/laapp-react/precache-manifest.dd71a893cdda3c2acfebaf022e1dfeef.js"
+  "/themes/laapp-react/precache-manifest.7200b24c4ebda67b7b3de36755e65dc0.js"
 );
 
 self.addEventListener('message', (event) => {
